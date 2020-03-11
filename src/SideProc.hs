ad0dbb3cec55b8ed24e5044794435bee2a4edb6d
Jan Koniarik <jan.koniarik@trustica.cz>
2020-03-11 11:50:03 +0100

    make the subthread non-blocking

--- src/SideProc.hs
+++ src/SideProc.hs
@@ -1,5 +1,7 @@
 module SideProc where
 
+import Control.Concurrent (forkIO, threadDelay, killThread)
+import          Control.Concurrent.MVar (newEmptyMVar, tryTakeMVar, putMVar)
 import           System.IO      (Handle, hGetLine, hIsEOF, hWaitForInput)
 import           System.Process (ProcessHandle, runInteractiveCommand)
 
@@ -11,6 +13,28 @@ data SideProcess = SideProcess {
 
 readLines :: Handle -> IO [String]
 readLines hndl = do
+    m_var <- newEmptyMVar
+    tid <- forkIO $ do
+        is_eof <- hIsEOF hndl
+        if is_eof
+        then return ()
+        else do
+            line <- hGetLine hndl
+            putMVar m_var line
+
+    threadDelay 20
+    killThread tid
+
+    mb_line <- tryTakeMVar m_var
+    case mb_line of
+        Just line -> do
+            sub_lines <- readLines hndl
+            return (line:sub_lines)
+        Nothing -> return []
+
+
+readLines2 :: Handle -> IO [String]
+readLines2 hndl = do
     is_eof <- hIsEOF hndl
     if is_eof
     then return []
@@ -20,7 +44,7 @@ readLines hndl = do
         then return []
         else do
             line <- hGetLine hndl
-            sub <- readLines hndl
+            sub <- readLines2 hndl
             return ( line : sub )
 
 runSideProcess :: String -> IO SideProcess
